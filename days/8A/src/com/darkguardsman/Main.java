ff9f9878abe862a28980701b2431307fb95c96db
Robert WC Seifert <rseifert.phone@gmail.com>
2018-12-10 11:26:42 -0500

    Sync

---
+++ days/8A/src/com/darkguardsman/Main.java
@@ -0,0 +1,28 @@
+package com.darkguardsman;
+
+import com.darkguardsman.helpers.FileHelpers;
+
+import java.io.File;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 12/10/18.
+ */
+public class Main {
+
+    public static void main(String... args) {
+        //get args
+        final File file = new File(args[0]);
+
+        //Arg output
+        System.out.println("File: " + file);
+
+        //Read file
+        System.out.println("\nReading File: ");
+        final List<String> lines = FileHelpers.getLines(file);
+    }
+}

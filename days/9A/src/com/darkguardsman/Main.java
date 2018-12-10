c9a47970a8d4e1a4ac09c8f9c0821e16962de8d7
Robert WC Seifert <rseifert.phone@gmail.com>
2018-12-10 12:32:16 -0500

    Sync

---
+++ days/9A/src/com/darkguardsman/Main.java
@@ -0,0 +1,12 @@
+package com.darkguardsman;
+
+/**
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 12/10/18.
+ */
+public class Main {
+
+    public static void main(String... args) {
+
+    }
+}
